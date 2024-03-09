a8a3f21f83d3c787f4169e84462c452adcb16816
Roberto Rojo <rrojo.va@gmail.com>
2024-03-09 12:46:49 +0100

    Create MVVM Structure

---
+++ NotesWithTesting/App/NotesWithTestingApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesWithTestingApp.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 9/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NotesWithTestingApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ NotesWithTesting/UI/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 9/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MainView: View {
+
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+#Preview {
+    MainView()
+}
