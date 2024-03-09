358ec423f352735b18541e95d396cee3d889ecee
Roberto Rojo <rrojo.va@gmail.com>
2024-03-09 12:42:04 +0100

    Initial Commit

---
+++ NotesWithTesting/NotesWithTestingApp.swift
@@ -0,0 +1,32 @@
+//
+//  NotesWithTestingApp.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 9/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NotesWithTestingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
