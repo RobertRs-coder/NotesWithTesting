3f00d6e9a891f902d21e54819bf0581749351031
Roberto Rojo <rrojo.va@gmail.com>
2024-03-09 13:15:10 +0100

    MainView navigation

--- NotesWithTesting/UI/Main/MainView.swift
+++ NotesWithTesting/UI/Main/MainView.swift
@@ -9,12 +9,48 @@ import SwiftUI
 import SwiftData
 
 struct MainView: View {
-
+    
+    @StateObject var viewModel: NotesViewModel = .init()
+    
+    //    init() {
+    //        self._viewModel = StateObject(wrappedValue: NotesViewModel())
+    //    }
+    
     var body: some View {
-        Text("Hi")
+        NavigationStack {
+            List {
+                ForEach(viewModel.notes) { note in
+                    NavigationLink(value: note) {
+                        VStack(alignment: .leading) {
+                            Text(note.title)
+                                .foregroundStyle(.primary)
+                            Text(note.getText)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Notes")
+            
+        }
     }
 }
 
 #Preview {
-    MainView()
+    MainView(
+        viewModel: NotesViewModel(
+            notes: [
+                .init(
+                    title: "First Note",
+                    text: "Testing first note",
+                    createdAt: .now
+                ),
+                .init(
+                    title: "Second Note",
+                    text: "Testing second note",
+                    createdAt: .now
+                )
+            ]
+        )
+    )
 }
