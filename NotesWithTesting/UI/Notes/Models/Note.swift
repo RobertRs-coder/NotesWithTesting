4676cdeee0d4a1ebbe696639cae954b34a2fd842
Roberto Rojo <rrojo.va@gmail.com>
2024-03-09 12:51:02 +0100

    Create Note model

---
+++ NotesWithTesting/UI/Notes/Models/Note.swift
@@ -0,0 +1,26 @@
+//
+//  Note.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 9/3/24.
+//
+
+import Foundation
+
+struct Note: Identifiable, Hashable {
+    let id: UUID
+    let title: String
+    let text: String?
+    let createdAt: Date
+    
+    var getText: String {
+        text ?? ""
+    }
+    
+    init(id: UUID = UUID(), title: String, text: String?, createdAt: Date) {
+        self.id = id
+        self.title = title
+        self.text = text
+        self.createdAt = createdAt
+    }
+}
