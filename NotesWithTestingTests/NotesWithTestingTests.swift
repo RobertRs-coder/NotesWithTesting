d7c5d2eedfe503015e6ce7d11faaa1d52b8b2475
Roberto Rojo <rrojo.va@gmail.com>
2024-03-11 13:12:24 +0100

    First test

---
+++ NotesWithTestingTests/NotesWithTestingTests.swift
@@ -0,0 +1,35 @@
+//
+//  NotesWithTestingTests.swift
+//  NotesWithTestingTests
+//
+//  Created by Roberto Rojo Sahuquillo on 11/3/24.
+//
+
+import XCTest
+@testable import NotesWithTesting
+
+// Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+// Naming Structure: test_[struct or class]_[variable or function]_[expected result]
+// Testing structure: Given, When, Then || Arrange, Act, Assert
+
+final class NotesWithTestingTests: XCTestCase {
+
+    
+    func test_Note_init_shouldBeSuccessful() {
+        // Given
+        let title = "Test title"
+        let text = "Test text"
+        let date = Date()
+        
+        //When
+        let note = Note(title: title, text: text, createdAt: date)
+        
+        // Then
+        XCTAssertEqual(note.title, title)
+        XCTAssertEqual(note.text, text)
+        XCTAssertEqual(note.createdAt, date)
+        
+    }
+
+
+}
