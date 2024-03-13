c099553ee89d3bd0e9aafd59ccd39f8244517180
Roberto Rojo <rrojo.va@gmail.com>
2024-03-13 13:11:01 +0100

    UseCases in domain

---
+++ NotesWithTesting/Domain/CreateNoteUseCase.swift
@@ -0,0 +1,48 @@
+//
+//  CreateNoteUseCase.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 13/3/24.
+//
+
+import Foundation
+
+struct CreateNoteUseCase {
+    var notesDatabase: NotesDatabaseProtocol
+    
+    init(notesDatabase: NotesDatabaseProtocol = NotesDatabase.shared) {
+        self.notesDatabase = notesDatabase
+    }
+    
+    func createNote(title: String, text: String) async throws {
+        let note: Note = .init(id: .init(),title: title, text: text, createdAt: .now)
+        
+        try await notesDatabase.insert(note: NoteDAO(identifier: note.id,title: note.title, text: note.text, createdAt: note.createdAt))
+    }
+    
+}
+
+
+/*
+ Implement mappers
+ -----------------
+ protocol CharacterModelToCollectionCellModelMapperProtocol {
+     func map(_ characters: CharactersModel?) -> [CollectionCellModel]
+     func map(_ character: CharacterModel?) -> CollectionCellModel?
+ }
+
+ class CharacterModelToCollectionCellModelMapper: CharacterModelToCollectionCellModelMapperProtocol {
+     func map(_ characters: CharactersModel?) -> [CollectionCellModel] {
+         guard let characters = characters else { return [] }
+         return characters.compactMap { map($0)
+         }
+     }
+     
+     func map(_ character: CharacterModel?) -> CollectionCellModel? {
+         guard let character = character else { return nil }
+         return CollectionCellModel(image: character.image,
+                                    title: "\(character.name) \(character.lastname)")
+         }
+ }
+ 
+ */
---
+++ NotesWithTesting/Domain/FetchAllNotesUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  FetchAllNotesUseCase.swift
+//  NotesWithTesting
+//
+//  Created by Roberto Rojo Sahuquillo on 13/3/24.
+//
+
+import Foundation
+
+
+struct FetchAllNotesUseCase {
+    var notesDatabase: NotesDatabaseProtocol
+    
+    init(notesDatabase: NotesDatabaseProtocol = NotesDatabase.shared) {
+        self.notesDatabase = notesDatabase
+    }
+    
+    func fetchAll() async throws -> [Note] {
+        try await notesDatabase.fetchAll().map { Note(title: $0.title, text: $0.text, createdAt: $0.createdAt) }
+    }
+}
